c7e38dedd24e29f840cb377350e8432febb1b7aa
lfoster1150 <lfoster1150@gmail.com>
2021-08-09 11:35:26 -0500

    Tested field of play creation with 50 squares.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('YOOOOOOO')
+const field = document.querySelector('#field')
+const fieldSquares = field.children
+for (let i = 1; i <= 50; i++) {
+  const fieldSquare = document.createElement('div')
+  fieldSquare.classList.add('field-square')
+  fieldSquare.dataset.position = i
+  field.appendChild(fieldSquare)
+}
