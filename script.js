fad547242f1f51fa649cdddb8003cb5939927bf2
lfoster1150 <lfoster1150@gmail.com>
2021-08-09 13:42:11 -0500

    Added simple board set up function. Both teams will start in similar spots on opposite sides of the field

--- script.js
+++ script.js
@@ -12,38 +12,61 @@ let player1OccupiedCells = []
 let player2OccupiedCells = []
 // let player2TackleZone ​= [] /// giving an error for some reason. Will need to re-visit
 let currentTurnMovesRemaining = 0 // Will probably need to move inside a function ( counts down after every movement) //
-let player1StartingPositions = []
-let player2StartingPositions = []
-
+let player1StartingPositions = [
+  56, 146, 236, 326, 416, 58, 148, 238, 328, 418, 240
+]
+let player2StartingPositions = [
+  35, 125, 215, 305, 395, 33, 123, 213, 303, 393, 211
+]
+/// Body Parts ///
+let field = document.querySelector('#field')
+let fieldSquares = field.children
+console.log(fieldSquares)
 /// Functions ///
-const startGame = () => {
-  boardSetup()
-}
-/// Event Listeners ///
-
-const field = document.querySelector('#field')
-const fieldSquares = field.children
-for (let i = 1; i <= 15; i++) {
-  for (let j = 1; j <= 30; j++) {
-    const fieldSquare = document.createElement('div')
-    fieldSquare.classList.add('field-square')
-    fieldSquare.dataset.row = i
-    fieldSquare.dataset.column = j
-    fieldSquare.dataset.position = j + (i - 1) * 30
-    field.appendChild(fieldSquare)
+const createField = () => {
+  const field = document.querySelector('#field')
+  const fieldSquares = field.children
+  for (let i = 1; i <= 15; i++) {
+    for (let j = 1; j <= 30; j++) {
+      const fieldSquare = document.createElement('div')
+      fieldSquare.classList.add('field-square')
+      fieldSquare.dataset.row = i
+      fieldSquare.dataset.column = j
+      fieldSquare.dataset.position = j + (i - 1) * 30
+      field.appendChild(fieldSquare)
+    }
+  }
+  /// Used only for console logging
+  for (let i = 1; i <= fieldSquares.length; i++) {
+    fieldSquares[i - 1].addEventListener('click', function (event) {
+      console.log(
+        `Position: ${event.target.dataset.position}`,
+        `Row: ${event.target.dataset.row}`,
+        `Column: ${event.target.dataset.column}`
+      )
+    })
   }
 }
-
-for (let i = 1; i <= fieldSquares.length; i++) {
-  fieldSquares[i - 1].addEventListener('click', function (event) {
-    console.log(
-      `Position: ${event.target.dataset.position}`,
-      `Row: ${event.target.dataset.row}`,
-      `Column: ${event.target.dataset.column}`
-    )
-    console.log(event.target)
+const boardSetup = () => {
+  /// setup for team 1
+  for (let i = 0; i < 11; i++) {
     const gamePiece = document.createElement('div')
     gamePiece.classList.add('game-piece')
-    event.target.appendChild(gamePiece)
-  })
+    gamePiece.id = 'team1'
+    fieldSquares[player1StartingPositions[i] - 1].appendChild(gamePiece)
+  }
+  /// setup for team 2
+  for (let i = 0; i < 11; i++) {
+    const gamePiece = document.createElement('div')
+    gamePiece.classList.add('game-piece')
+    gamePiece.id = 'team2'
+    fieldSquares[player2StartingPositions[i] - 1].appendChild(gamePiece)
+  }
 }
+const startGame = () => {
+  createField()
+  console.log
+  boardSetup()
+}
+startGame()
+/// Event Listeners ///
